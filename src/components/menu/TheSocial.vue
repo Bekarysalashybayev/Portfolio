d6b47df4b79d952601dc823b286f51f27f2e455d
bekarysalashybayev <bekarysalashybaev@gmail.com>
2023-09-24 14:52:38 +0600

    fix

--- src/components/menu/TheSocial.vue
+++ src/components/menu/TheSocial.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="social">
-    <a href="https://github.com/Bekarysalashybayev" target="_blank">
+    <a href="https://github.com/Bekarysalashybayev" aria-label="git" target="blank">
       <git-icon />
     </a>
 
-    <a href="https://www.linkedin.com/in/bekarys-alashybaev-155862158" target="_blank">
+    <a
+      href="https://www.linkedin.com/in/bekarys-alashybaev-155862158"
+      aria-label="linkedin"
+      target="blank"
+    >
       <in-icon />
     </a>
 
-    <a href="https://hh.kz/resume/7ad824c5ff05b690e40039ed1f325857616262" target="_blank">
+    <a
+      href="https://hh.kz/resume/7ad824c5ff05b690e40039ed1f325857616262"
+      aria-label="hh"
+      target="blank"
+    >
       <h-h-icon />
     </a>
   </div>
--- src/components/ui/BurgerButton.vue
+++ src/components/ui/BurgerButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="onClick">
+  <button @click="onClick" aria-label="burger button">
     <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clip-path="url(#b-button)">
         <path
