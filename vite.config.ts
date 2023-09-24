9ad624cf54e32a4d715884775f3ee823fabd6af5
bekarysalashybayev <bekarysalashybaev@gmail.com>
2023-09-24 15:01:25 +0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-import {fileURLToPath, URL} from 'node:url'
-
-import {defineConfig} from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import viteCompression from 'vite-plugin-compression'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    server: {
-        host: 'localhost',
-        port: 3000
+  plugins: [vue(), viteCompression()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  server: {
+    host: 'localhost',
+    port: 3000
+  }
 })
