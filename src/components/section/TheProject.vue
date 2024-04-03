3900eb243313f4cfa9f64249a29e08bf7c48caa4
bekarysalashybayev <bekarysalashybaev@gmail.com>
2024-04-03 23:44:00 +0500

    added projects

--- src/components/section/TheProject.vue
+++ src/components/section/TheProject.vue
@@ -1,24 +1,76 @@
 <template>
-  <div class="title">Projects</div>
+  <div class="title">Some of the Projects</div>
   <div class="projects">
-    <div class="project" v-for="i in 4" :key="i">
-      <div class="img"></div>
+    <div class="project">
+      <div class="img">
+        <img src="@/assets/img/project-1.png" alt="" />
+      </div>
       <div class="name">
-        Project {{ i }}
+        testhub.kz
         <div class="line"></div>
       </div>
       <div class="text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dolor ducimus eum fugit,
-        nam nemo nostrum nulla odio porro provident quam reprehenderit voluptatem. Ad enim eum non
-        placeat ut. Aliquid at atque, dicta expedita illo illum iusto laborum maiores minima
+        Our site provides unique tasks, based on official standards. Increase your level of
+        preparation, track progress and achieve high results. Exam confidence starts here!
       </div>
-      <be-button>Look it up</be-button>
+      <a href="https://testhub.kz/" target="blank">Look it up</a>
+    </div>
+    <div class="project">
+      <div class="img">
+        <img src="@/assets/img/project-2.png" alt="" />
+      </div>
+      <div class="name">
+        ent.testhub.kz
+        <div class="line"></div>
+      </div>
+      <div class="text">
+        Our site provides unique tasks, based on official standards. Increase your level of
+        preparation, track progress and achieve high results. Exam confidence starts here!
+      </div>
+      <a href="https://ent.testhub.kz/" target="blank">Look it up</a>
+    </div>
+    <div class="project">
+      <div class="img">
+        <img src="@/assets/img/project-3.png" alt="" />
+      </div>
+      <div class="name">
+        koreanstreetfood.kz
+        <div class="line"></div>
+      </div>
+      <div class="text">
+        Our motto: «Healthy.Delicious.Easy.» It is important to us that the flavor and quality of
+        our food is comfortable for our guests.
+      </div>
+      <a href="https://koreanstreetfood.kz/" target="blank">Look it up</a>
+    </div>
+    <div class="project">
+      <div class="img">
+        <img src="@/assets/img/project-4.png" alt="" />
+      </div>
+      <div class="name">
+        juz40test.kz
+        <div class="line"></div>
+      </div>
+      <div class="text">
+        The #1 course that works for results. All subjects are taught by teachers who have become
+        real specialists in their field.
+      </div>
+      <a href="https://koreanstreetfood.kz/" target="blank">Look it up</a>
+    </div>
+    <div class="project">
+      <div class="img">
+        <img src="@/assets/img/project-5.png" alt="" />
+      </div>
+      <div class="name">
+        Corporate project
+        <div class="line"></div>
+      </div>
+      <div class="text">Car with mileage on credit. Get approval online in 2 minutes</div>
+      <a href="https://car.eubank.kz/ru" target="blank">Look it up</a>
     </div>
   </div>
 </template>
-<script setup lang="ts">
-import BeButton from '@/components/ui/BeButton.vue'
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss">
 @use '@/assets/scss/colors' as *;
@@ -28,6 +80,16 @@ import BeButton from '@/components/ui/BeButton.vue'
   line-height: 1;
   font-weight: 600;
   margin-bottom: 5rem;
+}
+
+a {
+  font-size: 1.8rem;
+  border: 0;
+  color: inherit;
+  background: linear-gradient(to left, #13adc7, #6978d1, #945dd6);
+  width: max-content;
+  border-radius: 2rem;
+  padding: 1rem 2rem;
 }
 
 .projects {
@@ -64,7 +126,7 @@ import BeButton from '@/components/ui/BeButton.vue'
 
     .text {
       font-size: 1.6rem;
-      margin-bottom: 2rem;
+      margin-bottom: 3rem;
     }
 
     .img {
